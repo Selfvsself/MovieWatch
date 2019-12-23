bedf16f7501103fd6e01cfbf01e958c1dee9b74b
Self vs Self <killerman323@gmail.com>
2019-12-23 17:56:37 +0300

    1

--- app/src/main/java/com/selfvsself/moviewatch/MainActivity.java
+++ app/src/main/java/com/selfvsself/moviewatch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.selfvsself.moviewatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,56 +9,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnShow;
-
+    private BottomSheetBehavior mbottomSheetBehavior;
+    private TextView textViewState;
+    private Button btnExpand, btnCollapse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnShow = findViewById(R.id.button);
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        mbottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
 
-        btnShow.setOnClickListener(new View.OnClickListener() {
+        textViewState = findViewById(R.id.text_state);
+        btnExpand = findViewById(R.id.btn_expand);
+        btnCollapse = findViewById(R.id.btn_collapse);
+
+        btnExpand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
-                bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog);
+                mbottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
-                final EditText et_username = bottomSheetDialog.findViewById(R.id.et_username);
-                final EditText et_pass = bottomSheetDialog.findViewById(R.id.et_pass);
-                Button btn_submit = bottomSheetDialog.findViewById(R.id.button2);
-
-                btn_submit.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (et_username.getText().toString().equals("admin") &&
-                        et_pass.getText().toString().equals("admin")) {
-                            final AlertDialog.Builder bulder = new AlertDialog.Builder(v.getContext());
-                            bulder.setTitle("Login Succesful");
-                            bulder.setMessage("Welcome to Android");
-
-                            bulder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-
-                            AlertDialog alertDialog = bulder.create();
-                            alertDialog.show();
-                        } else {
-                            Toast.makeText(v.getContext(), "Login failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-                bottomSheetDialog.show();
             }
         });
+
+        btnCollapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mbottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+            }
+        });
+
+        mbottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                textViewState.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+                textViewState.setText(String.valueOf(v));
+            }
+        });
+
     }
 }
